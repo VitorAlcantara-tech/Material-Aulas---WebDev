b25b3401a05ae3d40dd69fd6599f3bf80f84bb2d
Vitor <vitoralcan5@gmail.com>
2025-09-01 22:40:38 -0300

    .

--- Aula 5 - Imagens e Rotas/react-tailwind/src/components/Sidebar.jsx
+++ Aula 5 - Imagens e Rotas/react-tailwind/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
       <h2 className="text-2xl font-bold mb-8">Dashboard</h2>
       <nav className="flex flex-col gap-3">
         <Link to='/' className="hover:bg-gray-700 p-2 rounded">Home</Link>
-        <Link to='/usuarios' className="hover:bg-gray-700 p-2 rounded">Usuários </Link>
+        <Link to='/usuario' className="hover:bg-gray-700 p-2 rounded">Usuários </Link>
         <Link to='/configuracoes' className="hover:bg-gray-700 p-2 rounded">Configurações</Link>
         <Link to='/relatorios' className="hover:bg-gray-700 p-2 rounded">Relatórios</Link>
 
