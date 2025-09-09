6cf883d2fe4d84c8295fd3976c1f44caa45ee638
Vitor <Vitor@gmail.com>
2025-09-08 22:38:21 -0300

    .

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function Usuarios() {
+  
+  const [usuarios, setUsuarios] = useState([]);
+  
+  //fetch puxa dados de algum lugar
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json()) // transforma em json
+      .then(data => setUsuarios(data)) // guarda o json em data
+      .catch(error => console.log(error)) // se der erro 
+      .finally(console.log("Acabou!"))
+  },[])
+    
+
+
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Usuários</h1>
-      <p className="mt-2 text-gray-600">Página de usuários.</p>
+      {usuarios.map(pegaItem => (
+        <div key={pegaItem.id}>
+          <p>Nome: {pegaItem.name}</p>
+          <p>Email: {pegaItem.email}</p>
+          <p>Rua: {pegaItem.address.street}</p>
+          <p>Latitude: {pegaItem.address.geo.lat}</p>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
